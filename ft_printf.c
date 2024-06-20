b265bec9c17f6b2d72688cb458578c20793b7428
Thibault Mazan <tmazan@made-f0Ar11s1.clusters.42paris.fr>
2024-06-20 13:00:30 +0200

    final push 20/06

--- ft_printf.c
+++ ft_printf.c
@@ -1,54 +1,71 @@
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdarg.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmazan <tmazan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/06 17:32:35 by ddifalla          #+#    #+#             */
+/*   Updated: 2024/06/20 12:59:27 by tmazan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int ft_printf(const char *, ...)
+#include "ft_printf.h"
+
+int	ft_testers(int *pj, va_list args, const char *format, int i)
 {
-    if (/*character actuel*/ == %)
-        /*character actuel*/++;
-            if (/*character actuel*/ == 'c')
-            {
-                ft_putchar(/*arg actuel*/);
-                /*arg actuel*/++;
-            }
-            if (/*character actuel*/ == 's')
-            {
-                ft_putstr(/*arg actuel*/);
-                /*arg actuel*/++;
-            }
-            if (/*character actuel*/ == 'p')
-            {
-                ft_putarghexa(/*arg actuel*/);
-                /*arg actuel*/++;
-            }
-            if (/*character actuel*/ == 'd')
-            {
-                ft_putnbrdec(/*arg actuel*/);
-                /*arg actuel*/++;
-            }
-            if (/*character actuel*/ == 'i')
-            {
-                ft_putintdec(/*arg actuel*/);
-                /*arg actuel*/++;
-            }
-            if (/*character actuel*/ == 'u')
-            {
-                ft_putunsnbrdec(/*arg actuel*/);
-                /*arg actuel*/++;
-            }
-            if (/*character actuel*/ == 'x')
-            {
-                ft_putnbrhexlower(/*arg actuel*/);
-                /*arg actuel*/++;
-            }
-            if (/*character actuel*/ == 'X')
-            {
-                ft_putnbrhexupper(/*arg actuel*/);
-                /*arg actuel*/++;
-            }
-            if (/*character actuel*/ == '%')
-            {
-                ft_putchar('%');
-                /*arg actuel*/++;
-            }
+	i++;
+	ft_regroupft(pj, args, format[i], 1);
+	ft_regroupft2(pj, args, format[i], 1);
+	return (i);
+}
+
+void	ft_regroupft(int *pj, va_list args, const char formatc, int fd)
+{
+	if (formatc == 'c')
+		ft_putchar_fd(va_arg(args, int), pj, fd);
+	else if (formatc == 's')
+		ft_putstr_fd(va_arg(args, char *), pj, fd);
+	else if (formatc == 'p')
+		ft_putnbr_base(\
+			va_arg(args, unsigned long long), "0123456789abcdef", pj, formatc);
+	else if (formatc == 'd' || formatc == 'i')
+		ft_putnbr_fd(va_arg(args, int), pj, fd);
+}
+
+void	ft_regroupft2(int *pj, va_list args, const char formatc, int fd)
+{
+	if (formatc == 'u')
+		ft_putunsnbr_fd(va_arg(args, unsigned int), pj, fd);
+	else if (formatc == 'x')
+		ft_putnbr_base(\
+			va_arg(args, unsigned int), "0123456789abcdef", pj, formatc);
+	else if (formatc == 'X')
+		ft_putnbr_base(\
+			va_arg(args, unsigned int), "0123456789ABCDEF", pj, formatc);
+	else if (formatc == '%')
+		ft_putchar_fd('%', pj, fd);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	int		i;
+	int		j;
+	va_list	args;
+
+	va_start(args, format);
+	i = 0;
+	j = 0;
+	while (format[i])
+	{
+		if (format[i] == '%')
+			i = ft_testers(&j, args, format, i) + 1;
+		else
+		{
+			ft_putchar_fd(format[i], &j, 1);
+			i++;
+		}
+	}
+	va_end(args);
+	return (j);
 }
